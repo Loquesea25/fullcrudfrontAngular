df5ec4f8fe3d1203cf9abb4c02614c5672d055eb
jorge <yeyemmp@gmail.com>
2023-04-02 00:01:00 -0500

    Front en creacion del formulario, inconveniente al crearlo, PENDIENTE

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { CiudadService } from './services/ciudad/ciudad.service';
+import { PaisesService } from './services/pais/pais.service';
+import { PersonaService } from './services/persona/persona.service';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'fullcrud_front';
+export class AppComponent implements OnInit{
+  
+
+  personaForm: FormGroup | undefined;
+
+  constructor(
+    public fb: FormBuilder,
+    public ciudadService: CiudadService,
+    public paisService: PaisesService,
+    public personaService: PersonaService
+    ){
+
+    }
+
+  ngOnInit(): void {
+    this.personaForm = this.fb.group({
+      namePersona : ['', Validators.required],
+		lastNamePersona : ['', Validators.required],
+		agePersona : ['', Validators.required],
+		emailPersona : ['', Validators.required],
+		pais : ['', Validators.required],
+		ciudad : ['', Validators.required]
+    })
+  }
 }
+
+
